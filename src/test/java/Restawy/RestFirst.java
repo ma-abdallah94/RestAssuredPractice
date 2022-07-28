40eeb043257d0079954ec0844eee3b25418ab6c1
ma-abdallah94 <85710599+ma-abdallah94@users.noreply.github.com>
2022-07-29 00:42:34 +0200

    First Commit

---
+++ src/test/java/Restawy/RestFirst.java
@@ -0,0 +1,15 @@
+package Restawy;
+import io.restassured.RestAssured;
+import org.testng.annotations.Test;
+
+public class RestFirst
+{
+@Test
+public void test() {
+    RestAssured
+            .given()
+            .when()
+            .then();
+}
+
+}
