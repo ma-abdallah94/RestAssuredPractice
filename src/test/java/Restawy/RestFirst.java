0363bfbef977b31c4be38dff9a069f223d79b219
ma-abdallah94 <85710599+ma-abdallah94@users.noreply.github.com>
2022-07-29 13:19:46 +0200

    0

--- src/test/java/Restawy/RestFirst.java
+++ src/test/java/Restawy/RestFirst.java
@@ -1,15 +1,19 @@
 package Restawy;
-import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
 import org.testng.annotations.Test;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
 
 public class RestFirst
 {
 @Test
 public void test() {
-    RestAssured
-            .given()
-            .when()
-            .then();
+
+        given().baseUri("https://62e313f0b54fc209b88492c8.mockapi.io/api/v1/")
+                .when().get("Tasks")
+                .then().log().all()
+                .assertThat().statusCode(200);
+
 }
 
 }
