72c55e36e9691f7200541a2d827470d984dc3744
ma-abdallah94 <85710599+ma-abdallah94@users.noreply.github.com>
2022-07-31 21:05:22 +0200

    No Reason

--- src/test/java/Restawy/RestFirst.java
+++ src/test/java/Restawy/RestFirst.java
@@ -1,5 +1,7 @@
 package Restawy;
 import static io.restassured.RestAssured.*;
+
+import groovyjarjarantlr4.v4.runtime.misc.NotNull;
 import org.testng.annotations.Test;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
@@ -7,13 +9,23 @@ import static org.hamcrest.Matchers.*;
 public class RestFirst
 {
 @Test
-public void test() {
+public void test1() {
 
         given().baseUri("https://62e313f0b54fc209b88492c8.mockapi.io/api/v1/")
                 .when().get("Tasks")
-                .then().log().all()
-                .assertThat().statusCode(200);
-
+                .then().log().all().assertThat().statusCode(200)
+                //.assertThat().body("[0].name" , equalTo("Ms. Bessie Wiegand"))
+                .assertThat().body("name" , hasItems()) ;
 }
+
+@Test
+        public void test2() {
+
+                given().baseUri("https://62e313f0b54fc209b88492c8.mockapi.io/api/v1/")
+                        .when().get("Tasks")
+                        .then().log().all().assertThat().statusCode(200)
+                        .assertThat().body("[0].name" , equalTo("Ms. Bessie Wiegand"));
+
+        }
 
 }
